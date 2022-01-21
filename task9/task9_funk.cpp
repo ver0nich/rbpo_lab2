04b978c9ca65d8acf0536d172c5d24e16d0ff3be
Veronika <90419801+ver0nich@users.noreply.github.com>
2022-01-21 21:09:38 +0300

    second

--- task9/task9_funk.cpp
+++ task9/task9_funk.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
 #include "task9_zotova.h"
 double Zotova::x, Zotova::y, Zotova::result;
-void Zotova::f();
+ void Zotova::f() {
+	result = pow((cos(x) - cos(y)), 2) - pow((sin(x) - sin(y)), 2);
+};
